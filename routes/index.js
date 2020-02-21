4b79186b8e48e4141cde835aa5c16e45416224dc
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-02-21 13:12:49 +0530

    Complete node.js part

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,97 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var path = require('path');
+var fs = require('fs');
+var rl = require('readline');
+
+// SET STORAGE
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'uploads')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+});
+
+// Get args
+var myArgs = process.argv.slice(2);
+var files = [];
+
+for (index in myArgs) {
+	var file = myArgs[index];
+	var readInterface = rl.createInterface({
+		input: fs.createReadStream(file),
+		output: process.stdout,
+		console: false
+	});
+	readInterface.on('line', function(line) {
+		files.push(line);
+	});
+}
+
+/* Get unique elements only */
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+function formRow(icon, name, link) {
+	return '<tr>\
+	<td><span uk-icon="'+icon+'"></span></td>\
+	<td>'+name+'</td>\
+	<td><form action="/download" method="POST"><button type="submit" name="file" value="'+link+'"class="uk-button uk-button-primary uk-align-right" style="margin: 0;">Download <span uk-icon="download"></button></form></td>\
+	</tr>';
+}
+
+var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var list = files.filter( onlyUnique );
+	var code;
+	if( list.length > 0 )
+		{
+			var rows = list.map( function(value) {
+			var name = value.substring(value.lastIndexOf('/') + 1);
+			var ext = value.substring(value.indexOf('.') + 1) == value ? 'null' : value.substring(value.indexOf('.') + 1);
+			var icon = 'file';
+			if( ext.match('jpg|jpeg|png|dng|bmp|tiff') ) icon = 'image' ;
+			if( ext.match('mp3|ogg|avi|mp4|flac') ) icon = 'play' ;
+			if( ext.match('txt|doc|docx') ) icon = 'file-text' ;
+			if( ext.match('pdf') ) icon = 'file-pdf' ;
+			return formRow(icon, name, value);
+		});
+		code = "<table class=\"uk-table uk-table-divider\">";
+		rows.forEach(function(value){
+			code += "\n" + value;
+		});
+		code += "</table>"
+	}
+	else {
+		code = '<table class="uk-table uk-table-divider">\
+		<tr><td>\
+		No files selected to share\
+		</td></tr>\
+		</table>';
+	}
+	res.render('index', { code: code });
+});
+
+/* POST home page. */
+router.post('/', upload.array('files[]'), (req, res) => {
+	const files = req.files
+	if (!files) {
+		const error = new Error('Please choose files')
+		error.httpStatusCode = 400
+		return next(error)
+	}
+ 
+		res.send(files)
+});
+
+router.post('/download', (req, res) => {
+	res.download(req.body.file);
 });
 
 module.exports = router;
