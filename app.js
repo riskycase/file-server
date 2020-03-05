ed7b4175d4cb774565ccc4b0066a97b1f28b87a0
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-03-05 18:45:03 +0530

    Fix errors shown on code factor

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fs = require('fs');
-var rl = require('readline');
 var zip = require('express-easy-zip');
 
 var indexRouter = require('./routes/index');
@@ -31,7 +29,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
