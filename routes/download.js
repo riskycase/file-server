a8eaa083ce49f3bbfc745ea719309cc1aa0a9a5b
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-04-21 19:13:31 +0530

    Fixes added on 21st April 2020

--- routes/download.js
+++ routes/download.js
@@ -1,31 +1,33 @@
 var router = require('express').Router();
 var storage = require('../middleware/storage');
 
+// Get the name of the file/folder from the path
+function nameOf(path) {
+	return (path.lastIndexOf('/')+1 === path.length) ? path.substring(path.lastIndexOf('/', path.lastIndexOf('/') - 1 ) + 1, path.length - 1) : name = path.substring(path.lastIndexOf('/') + 1);
+}
+
 /* Download single file */
 router.post('/', (req, res) => {
-	var value = storage.getPaths()[req.body.file];
-	if ( value.lastIndexOf('/')+1 === value.length ) {
-		var name = value.substring(value.lastIndexOf('/', value.lastIndexOf('/') - 1 ) + 1, value.length - 1);
+	var path = storage.getPaths()[req.body.file];
+	if ( path.lastIndexOf('/')+1 === path.length ) {
+		var name = nameOf(path);
 		var namezip = name + '.zip';
 		res.zip({
 			'files':[
-				{ 'path': value, 'name': name }
+				{ 'path': path, 'name': name }
 			],
 			'filename' : namezip
 		});
 	}
 	else {
-		res.download(value);
+		res.download(path);
 	}
 });
 
 /* Download all files */
 router.get('/', (req, res) => {
 	var filesJSON = storage.getPaths().map( function(value) {
-		if ( value.lastIndexOf('/')+1 === value.length )
-			var name = value.substring(value.lastIndexOf('/', value.lastIndexOf('/') - 1 ) + 1, value.length - 1);
-		else
-			var name = value.substring(value.lastIndexOf('/') + 1);
+		var name = nameOf(value);
 		return { 
 			'path': value,
 			'name': name,
