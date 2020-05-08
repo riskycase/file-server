a28bbfbfc11c550139e8acc25623587ae78ebb26
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-05-08 07:49:53 +0000

    Added changes related to ECMA and init script

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
-var router = require('express').Router();
-var multer = require('multer');
-var storage = require('../middleware/storage');
-
+const router = require('express').Router();
+const storage = require('../middleware/storage');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	var rows = storage.getRows();
+	const rows = storage.getRows();
 	if(rows.length > 0) res.render('index', { lines : rows });
 	else res.render('index', { lines : undefined });
 });
 
 /* POST home page. */
 router.post('/', storage.saveFiles , (req, res, next) => {
-	var files = req.files;
+	const files = req.files;
 	if (!files) {
 		const error = new Error('Please choose files');
 		error.status = 400;
